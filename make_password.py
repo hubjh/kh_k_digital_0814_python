cfc8d2da5928d707dacf94479dfae2baa6f044d8
hubjh <haha1057@naver.com>
2023-09-06 17:54:17 +0900

    feat : add Python program 230906 evening time

---
+++ make_password.py
@@ -0,0 +1,36 @@
+# 각 사이트 비밀번호 만들기
+# 규칙1 : http://naver.com에서 앞의 http:// 잘라내기
+# 규칙2 : 처음 만나는 점(.) 이후는 제외
+# 규칙3 : 남은 글자 중 처음 세자리 + 글자 갯수 + 글자에 포함된 'o' 갯수 +글자에 포함된 'k' 갯수+ '!' + 자신의이니셜(예: 'jks')
+
+# =========================== 내가 푼 것 ===============================
+# source_url = "http://naver.com"
+# find_str = "http://"
+# tmp_split = source_url.split(".")
+# find_result = tmp_split[0].find(find_str)
+# if find_result != -1:
+#     my_initial = input("자신의 이니셜을 넣어주세요 : ")
+#     front_url = tmp_split[0][:len(find_str)]
+#     other_url = tmp_split[0][len(find_str):]
+#     front_three = other_url[:3]
+#     str_len = str(len(other_url))
+#     count_o = str(other_url.count("o"))
+#     count_k = str(other_url.count("k"))
+#     print(f"{front_three}{str_len}{count_o}{count_k}!{my_initial}")
+# else:
+#     print(f"{find_str}가 없습니다.")
+
+# =========================== 강사 님이 푼 것 ===============================
+file_name = "password.txt"
+fd = open(file_name, "wt")
+while True:
+    url = input("사이트 : ")
+    if url == 'exit': break
+    my_str = url.replace("http://", "")
+    my_str = my_str[:my_str.index(".")] # 슬라이싱 : 처음부터 "." 인덱스 미만
+    password = (my_str[:3] + str(len(my_str)) + str(my_str.count("o")) + str(my_str.count("k"))
+                + "!" + "hjh")
+    print("비밀번호 : " + password)
+    fd.write(password + "\n")
+fd.close()
+
---
+++ python_230906_ex1.py
@@ -0,0 +1,70 @@
+# 1. 정해진 형식으로 시간을 입력 받아서 출력하기
+# 입력 형식 : 22:5:5 => 오후 10시 05분 05초
+# while True:
+#     time = input("시간을 입력해주세요 : ")
+#     if time == "exit":
+#         print("시간 입력을 종료합니다.")
+#         break
+#     else:
+#         hour, mint, sec = map(int, time.split(":"))
+#
+#         if hour >= 12:
+#             hour =  str((hour) - 12)
+#             print(f"오후 {hour:02}시 {mint:02}분 {sec:02}초")
+#         else:
+#             print(f"오전 {hour:02}시 {mint:02}분 {sec:02}초")
+
+# 2. 3개의 정수를 입력 받아 최대값과 최소값 구하기
+# while True:
+#     nums = input("정수을 입력해주세요 : ")
+#     if nums == "exit":
+#         print("정수 입력을 종료합니다.")
+#         break
+#     else:
+#         nums_list = list(map(int, nums.split(" ")))
+#         print(f"최소 : {min(nums_list)} 최대 : {max(nums_list)}")
+
+# 3. 주민등록번호를 입력받아 생년월일, 성별, 나이 출력하기
+from datetime import datetime
+
+while True:
+    curr_year = datetime.today().year
+    regi_num = input("주민등록번호를 입력해주세요 : ")
+    if regi_num == "exit":
+        print("주민등록번호 입력을 종료합니다.")
+        break
+    else:
+        year = regi_num[:6][:2]
+        mon = regi_num[:6][2:4]
+        day = regi_num[:6][4:6]
+        gender_num = regi_num[7]
+        print(gender_num)
+        print(f"curr_year = {curr_year}")
+        print(f"jumin = {(1900 + int(regi_num[:2]))}")
+        print(f"age = {curr_year - (1900 + int(regi_num[:2]))}")
+        print(f"age = {str(curr_year - (1900 + int(regi_num[:2])))}")
+        jumin_num = regi_num[:2]
+
+        if int(gender_num) in [1, 2, 5, 6]:
+            age = str(curr_year - (1900 + int(jumin_num)))
+        else:
+            age = str(curr_year - (2000 - int(jumin_num)))
+        # 940312 - 1265466
+        if gender_num == 1 or gender_num == 3:
+            gender = "M"
+        else:
+            gender = "W"
+
+        print(f"생년월일 : {year}년 {mon}월 {day}일 \n성별 : {gender} \n나이 : {age}")
+
+
+
+# 4. 갯수가 정해지지 않은 여러 개의 정수를 입력 받아 합계와 평균 구하기
+# while True:
+#     nums = input("정수을 입력해주세요 : ")
+#     if nums == "exit":
+#         print("정수 입력을 종료합니다.")
+#         break
+#     else:
+#         nums_list = list(map(int, nums.split(" ")))
+#         print(f"합계 : {sum(nums_list)} 평균 : {sum(nums_list)/len(nums_list)}")
