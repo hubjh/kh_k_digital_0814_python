b67f1417fa91084af42a33fde93c815c648e743c
hubjh <haha1057@naver.com>
2023-09-06 11:06:49 +0900

    feat : add Python program 230906 morning time

---
+++ test.py
@@ -0,0 +1,44 @@
+print("Hello, world")
+print("Hello, world")
+print(100)
+print(33.33)
+print(100_23)
+
+# 변수를 선언하고 값을 대입
+num = 100   # 데이터형은 값이 대입되는 순간에 결정 남
+print(num)
+print(type(num))
+
+# 여기는 주석입니다.
+"""
+여기는 범위 주석 구간입니다.
+ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹ
+"""
+
+# 변수 활용
+name = "곰돌이사육사"
+email = "what670@gmail.com"
+age = 25
+addr = "경기도 남양주시"
+
+print(f"""
+이름 : {name}
+이메일 : {email}
+나이 : {age}
+주소 : {addr}
+""")
+
+# Python은 boolean 값이 대문자로 시작한다.
+is_adult = True
+if is_adult :
+    print("나는 성인 입니다.")
+else :
+    print("나는 성인이 아닙니다.")
+
+# list = [1,2,3,4,5,6,7,8,9,10]
+# sum = 0
+# for i in list:
+#     sum += i
+#
+# print(sum)
+
