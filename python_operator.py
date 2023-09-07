4c4f63473530f0fdcf8759be7aac297d00343493
hubjh <haha1057@naver.com>
2023-09-07 09:54:00 +0900

    feat : add Python program 230907 morning time

---
+++ python_operator.py
@@ -0,0 +1,15 @@
+# 산술연산자 : 사칙연산자(+, -, *, /) //(몫), **(제곱연산자), %(나머지 연산자)
+i = 10 # 값을 대입할 때 데이터 형이 결정됨, 왜냐하면 파이썬은 데이터형이 없음
+j = 3
+print(f"덧셈 : {i + j}")
+print(f"뺄셈 : {i - j}")
+print(f"곱셈 : {i * j}")
+print(f"나눗셈 : {i / j}")
+print(f"몫 : {i // j}")
+print(f"나머지 : {i % j}")
+print(f"제곱연산 : {i ** j}")
+test = "Python !!!"
+print(test + "Hello")
+print(test + str(100))
+print(test * 3)     # test 문자열을 2번 반복하겠다는 의미
+
