b08111c92832d0502ff575a0eb86f306ddf5feeb
hubjh <haha1057@naver.com>
2023-09-07 17:47:39 +0900

    feat : add Python program 230907 evening time

--- python_230907_ex2.py
+++ python_230907_ex2.py
@@ -35,26 +35,30 @@
 
 
 # ======================= 숫자의 개수 ==========================
-# while True:
-#     a_inp = int(input("a (100 ~ 999) : "))
-#     if 100 <= a_inp < 1000:
-#         break
-#     else:
-#         print("숫자 a를 다시 입력해주세요.")
-# while True:
-#     b_inp = int(input("b (100 ~ 999) : "))
-#     if 100 <= b_inp < 1000:
-#         break
-#     else:
-#         print("숫자 b를 다시 입력해주세요.")
-# while True:
-#     c_inp = int(input("c (100 ~ 999) : "))
-#     if 100 <= c_inp < 1000:
-#         break
-#     else:
-#         print("숫자 c를 다시 입력해주세요.")
-#
-# rst_num = a_inp * b_inp * c_inp
+while True:
+    a_inp = int(input("a (100 ~ 999) : "))
+    if 100 <= a_inp < 1000:
+        break
+    else:
+        print("숫자 a를 다시 입력해주세요.")
+while True:
+    b_inp = int(input("b (100 ~ 999) : "))
+    if 100 <= b_inp < 1000:
+        break
+    else:
+        print("숫자 b를 다시 입력해주세요.")
+while True:
+    c_inp = int(input("c (100 ~ 999) : "))
+    if 100 <= c_inp < 1000:
+        break
+    else:
+        print("숫자 c를 다시 입력해주세요.")
+
+rst_num = a_inp * b_inp * c_inp
+ls = str(rst_num)
+for i in range(10) :
+    print(f'{str(i)} : {ls.count(str(i))}') # 해당 문자의 개수 반환
+
 # print(f"""result_number : {rst_num}
 # 0 : {str(rst_num).count('0')}개
 # 1 : {str(rst_num).count('1')}개
@@ -87,41 +91,41 @@
 
 
 # ==================== 통화 요금 ========================
-call_time_list = []
-while True:
-    call_cnt = int(input("통화의 개수 : "))
-    if 0 < call_cnt <= 20:
-        break
-    else:
-        print("통화 개수를 다시 입력해주세요.")
-
-for i in range(call_cnt):
-    print(f"입력된 통화 시간 수 : {i+1}")
-    while True:
-        call_time = int(input("통화 시간 입력 : "))
-        if 0 < call_time <= 10000:
-            call_time_list.append(call_time)
-            break
-        else:
-            print("통화 시간을 다시 입력 해주세요.")
-while True:
-    yogum = input("요금제 입력 [Y] [M]: ")
-    if yogum in ['y', 'Y', 'm', 'M']:
-        break
-    else:
-        print("요금제를 다시 입력해주세요.")
-
-total_usage = 0
-for clt in call_time_list:
-    if yogum == 'y' or yogum == 'Y':
-        if clt < 30:
-            total_usage += 10
-        else:
-            total_usage += 20
-    if yogum == 'm' or yogum == 'M':
-        if clt < 60:
-            total_usage += 15
-        else:
-            total_usage += 30
-
-print(f"total_usage : {total_usage}")
+# call_time_list = []
+# while True:
+#     call_cnt = int(input("통화의 개수 : "))
+#     if 0 < call_cnt <= 20:
+#         break
+#     else:
+#         print("통화 개수를 다시 입력해주세요.")
+#
+# for i in range(call_cnt):
+#     print(f"입력된 통화 시간 수 : {i+1}")
+#     while True:
+#         call_time = int(input("통화 시간 입력 : "))
+#         if 0 < call_time <= 10000:
+#             call_time_list.append(call_time)
+#             break
+#         else:
+#             print("통화 시간을 다시 입력 해주세요.")
+# while True:
+#     yogum = input("요금제 입력 [Y] [M]: ")
+#     if yogum in ['y', 'Y', 'm', 'M']:
+#         break
+#     else:
+#         print("요금제를 다시 입력해주세요.")
+#
+# total_usage = 0
+# for clt in call_time_list:
+#     if yogum == 'y' or yogum == 'Y':
+#         if clt < 30:
+#             total_usage += 10
+#         else:
+#             total_usage += 20
+#     if yogum == 'm' or yogum == 'M':
+#         if clt < 60:
+#             total_usage += 15
+#         else:
+#             total_usage += 30
+#
+# print(f"total_usage : {total_usage}")
